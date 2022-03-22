ab72178440f8242b557e8149ba3a46fc701d41e8
Jerry <defineantoine@gmail.com>
2022-03-22 19:20:23 +0000

    Inital Implementation - Works, Not Efficient

---
+++ LC - Group Anagrams.py
@@ -0,0 +1,62 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging
+ the letters of a different word or phrase, typically using all the 
+original letters exactly once.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+"""
+
+
+"""
+Big O: Hide your eyes. It works but it O(n Log n) time.
+"""
+
+def isAnagram(s, t):
+    result = 0
+    if len(s) != len(t):
+        return False
+    for x in range(0, len(s)):
+        result += hash(s[x])
+        result -= hash(t[x])
+    if result == 0:
+        return True
+    else:
+        return False
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        listSet = set()
+        newList, tempList = [], []
+        if len(strs) == 1:
+            return [strs]
+        for x in range(0, len(strs)):
+            if strs[x] in listSet:
+                continue
+            else:
+                listSet.add(strs[x])
+                tempList = [strs[x]]
+            for y in range(strs.index(strs[x]), len(strs)):
+                if strs[y] in listSet:
+                    if y == x:
+                        continue
+                    if isAnagram(strs[x], strs[y]):
+                        tempList.append(strs[y])
+                        continue
+                if isAnagram(strs[x], strs[y]):
+                    tempList.append(strs[y])
+                    listSet.add(strs[y])
+            newList.append(tempList)
+        return newList

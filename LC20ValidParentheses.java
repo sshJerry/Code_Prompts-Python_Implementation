379e971b6e1c56777581a7e8b4aebfaec2ccc4c4
sshJerry <defineantoine@gmail.com>
2025-08-19 00:32:54 -0400

    LC 20 Valid Parentheses

--- LC20ValidParentheses.java
+++ LC20ValidParentheses.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 /*
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
@@ -37,4 +40,28 @@ Constraints:
 s consists of parentheses only '()[]{}'.
  */
 public class LC20ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> bracketMap = new HashMap<>();
+        bracketMap.put('}', '{');
+        bracketMap.put(')', '(');
+        bracketMap.put(']', '[');
+
+        if (s.length() <= 1){
+            return false;
+        }
+        for (Character c : s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            }
+            if (c == '}' || c == ']' || c == ')') {
+                if (!stack.empty() && stack.peek() == bracketMap.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
 }
